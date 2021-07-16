a7aa9533303536ef772c8443356fa55d2ad6e660
SRBoyer <srb96@hotmail.com>
2021-07-16 00:56:38 -0400

    removed useless import

--- src/readexternaldata/Launcher.java
+++ src/readexternaldata/Launcher.java
@@ -1,18 +1,15 @@
 package readexternaldata;
 
 import javax.swing.SwingUtilities;
-import logic.WeightedRandomBag;
 import gui.*;
 
 public class Launcher {
 	
 	public static void main(String[] args) {
 		
-		
 		FirstGUI mainMenu = new FirstGUI();
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				
 				mainMenu.createGUI();
 			}
 		});
